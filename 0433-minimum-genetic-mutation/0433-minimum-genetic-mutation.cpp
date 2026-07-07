3cc4b67f3f099a579c69d5602ee79dd8702251ab
realishanrehan <74533998+Ishan5101@users.noreply.github.com>
2026-07-07 18:29:55 +0530

    Time: 0 ms (100%), Space: 8.9 MB (46.76%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        unordered_set<string> bankset(bank.begin(),bank.end());
+        unordered_set <string> bankset(bank.begin(),bank.end());
         unordered_set<string> visited;
-        queue<string> q;
 
+        queue<string> q;
         q.push(start);
-        visited.insert(start);
         int level=0;
+
         while(!q.empty()){
             int n=q.size();
             while(n--){
                 auto curr=q.front();
                 q.pop();
-if(curr==end) return level;
-                for(char ch: "ACGT"){
+                if(curr==end) return level;
+                for(char c:"ACGT"){
                     for(int i=0;i<curr.length();i++){
-                        string ishan=curr;
-                         ishan[i]=ch;
-
-                         if(visited.find(ishan) == visited.end() && bankset.find(ishan) != bankset.end()){
-                             visited.insert(ishan);
-                             q.push(ishan);
-                         }
+string ishan=curr;
+ishan[i]=c;
+                if(visited.find(ishan) == visited.end() && bankset.find(ishan)!=bankset.end()){
+                        visited.insert(ishan);
+                        q.push(ishan);
+                }
                     }
                 }
-            } level++;
+                
+            }
+            level++;
         }
 return -1;
     }
