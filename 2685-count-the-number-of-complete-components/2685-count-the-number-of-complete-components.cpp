96e60fadc97eb00f7b66ff153face4b334fa4435
realishanrehan <74533998+Ishan5101@users.noreply.github.com>
2026-07-11 10:36:34 +0530

    Time: 94 ms (10.94%), Space: 148.7 MB (5.65%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
-   int bfs(unordered_map<int, vector<int>>& adj, int p, int n, vector<int>& visited) {
-    queue<int> q;
+    int bfs(unordered_map<int, vector<int>>& adj, int p, int n,
+            vector<int>& visited) {
 
-    visited[p] = 1;
-    q.push(p);
+        queue<int> q;
 
-    int counter = 0;   
-    int edges = 0;    
+        visited[p] = 1;
+        q.push(p);
 
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
+        int counter = 0;
+        int edges = 0;
 
-        counter++;
-        edges += adj[u].size();
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
 
-        for (auto v : adj[u]) {
-            if (visited[v] == -1) {
-                visited[v] = 1;
-                q.push(v);
+            counter++;
+            edges += adj[u].size();
+
+            for (auto v : adj[u]) {
+                if (visited[v] == -1) {
+                    visited[v] = 1;
+                    q.push(v);
+                }
             }
         }
+
+        edges /= 2;
+
+        return edges == counter * (counter - 1) / 2;
     }
 
-    edges /= 2;
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
 
-    return edges == counter * (counter - 1) / 2;
-}
-    
+        for (auto vec : edges) {
+            int a = vec[0];
+            int b = vec[1];
+
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+
+        vector<int> visited(n, -1);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == -1) {
+                ans += bfs(adj, i, n, visited);
+            }
+        }
+
+        return ans;
+    }
 };
