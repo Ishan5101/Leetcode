3357129adab71bdee9ceff17ae360c6e3d47d0aa
realishanrehan <74533998+Ishan5101@users.noreply.github.com>
2026-07-16 22:36:17 +0530

    Time: 55 ms (96.96%), Space: 12.3 MB (89.25%) - LeetHub

---
+++ 0464-can-i-win/0464-can-i-win.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dp[1 << 20];
+
+    bool dfs(int M, int T, int K) {
+        if (T <= 0) return 0;
+
+        if (dp[K] != -1)
+            return dp[K];
+
+        for (int i = 0; i < M; ++i) {
+            if (!(K & (1 << i)) &&
+                !dfs(M, T - (i + 1), K | (1 << i))) {
+                return dp[K] = 1;
+            }
+        }
+
+        return dp[K] = 0;
+    }
+
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (desiredTotal == 0)
+            return 1;
+
+        int total = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
+
+        if (total < desiredTotal)
+            return 0;
+
+        if (total == desiredTotal)
+            return maxChoosableInteger % 2;
+
+        memset(dp, -1, sizeof(dp));
+
+        return dfs(maxChoosableInteger, desiredTotal, 0);
+    }
+};
