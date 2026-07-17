bd15c683d57d1fd0646e07bfb275d38543d834fe
realishanrehan <74533998+Ishan5101@users.noreply.github.com>
2026-07-17 23:58:44 +0530

    Time: 161 ms (31.72%), Space: 105.7 MB (32.18%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,45 +1,57 @@
 class Solution {
 public:
-    bool predec( string &prev,string & curr){
-        int m=prev.length();
-        int n=curr.length();
+    bool predec(string &prev, string &curr) {
+        int m = prev.length();
+        int n = curr.length();
 
-        if(m>=n || n-m!=1) return false;
-          int i = 0, j = 0;
-        while(i < m && j < n) {
-            if(prev[i] == curr[j]) {
+        if (n - m != 1)
+            return false;
+
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+            if (prev[i] == curr[j])
                 i++;
-            }
             j++;
         }
-        return i==m;
 
+        return i == m;
     }
-    int lis(int n,vector<string>words,int prev,int curr,vector<vector<string>>& dp){
-        if(curr==n) return 0;
-if(prev!=-1 && dp[prev][curr]!=-1) return dp[prev][curr];
-        int taken=0;
-        if(prev!=-1 || predec(words[prev],words[curr])){
-            taken= 1+ lis(n,words,curr,curr+1);
+
+    int lis(int n, vector<string>& words, int prev, int curr,
+            vector<vector<int>>& dp) {
+
+        if (curr == n)
+            return 0;
+
+        if (prev != -1 && dp[prev][curr] != -1)
+            return dp[prev][curr];
+
+        int taken = 0;
+
+        if (prev == -1 || predec(words[prev], words[curr])) {
+            taken = 1 + lis(n, words, curr, curr + 1, dp);
         }
 
-        int skip=lis(n,words,prev,curr+1);
+        int skip = lis(n, words, prev, curr + 1, dp);
 
+        if (prev != -1)
+            dp[prev][curr] = max(taken, skip);
 
-        if(prev != -1)
-            t[prev][curr] =  max(taken, skip);
-        
         return max(taken, skip);
     }
-       static bool ishan(string &s1,string& s2){
-            return s1.length()<s2.length();
-        }
+
+    static bool ishan(string &a, string &b) {
+        return a.length() < b.length();
+    }
+
     int longestStrChain(vector<string>& words) {
-        int n=words.size();
-vector<vector<string>> dp(n+1,vector<string> (n+1,-1));
-        sort(begin(words),end(words),ishan);
+        sort(words.begin(), words.end(), ishan);
 
-        return lis(n,words,-1,0,dp);
+        int n = words.size();
 
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return lis(n, words, -1, 0, dp);
     }
 };
