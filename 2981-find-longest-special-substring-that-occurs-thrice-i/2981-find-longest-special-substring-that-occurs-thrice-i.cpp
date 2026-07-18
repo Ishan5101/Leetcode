d091c6e16be7eacdd222ec71d9e62348c2bea9d5
realishanrehan <74533998+Ishan5101@users.noreply.github.com>
2026-07-19 02:13:44 +0530

    Time: 7 ms (74.11%), Space: 10.6 MB (83.85%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        int n=s.length();
-        unordered_map<string,int> mp;
-        for(int i=0;i<n;i++){
-            string curr="";
-            for(int j=i;j<n;j++){
-            if(curr.empty() || curr.back() == s[j]){
-                    curr+=s[j];
-                    mp[curr]++;
-                }else break;
-            }
-        }
-        int result=0;
-        for(auto it: mp){
-            string ishan=it.first;
-            int count=it.second;
-            if(count>=3 && ishan.length()>result){
-                result=ishan.length();
+        int n = s.length();
+
+        map<pair<char, int>, int> mp;
+
+        for(int i = 0; i < n; i++) {
+            char ch = s[i];
+            int l = 0;
+            for(int j = i; j < n; j++) {
+                if(s[j] == ch) {
+                    l++;
+                    mp[{ch, l}]++;
+                } else {
+                    break;
+                }
             }
         }
 
-        return result==0?-1 : result;
+
+        int result = 0;
+        for(auto &it : mp) {
+            int length = it.first.second;
+            int freq = it.second;
+
+            if(freq >= 3 && length > result) {
+                result = length;
+            }
+        }
+
+        return result == 0 ? -1 : result;
     }
 };
