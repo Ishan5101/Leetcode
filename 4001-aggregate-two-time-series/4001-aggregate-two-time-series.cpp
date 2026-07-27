f1d0d309aec0845ecc05cd7ac49b790fa98584cb
realishanrehan <74533998+Ishan5101@users.noreply.github.com>
2026-07-27 11:25:38 +0530

    Time: 78 ms (83.33%), Space: 350.4 MB (16.67%) - LeetHub

--- 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& series1,
-                                            vector<vector<int>>& series2) {
-
+    vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& series1, vector<vector<int>>& series2) {
         vector<vector<int>> ans;
-
-        int n = series1.size();
+        int n=series1.size();
         int m = series2.size();
-
-        int i = 0, j = 0;
-
-        while (i < n || j < m) {
-
-            int currentTime;
-
-            if (j == m || (i < n && series1[i][0] < series2[j][0])) {
-                currentTime = series1[i][0];
-            }
-            else if (i == n || series2[j][0] < series1[i][0]) {
-                currentTime = series2[j][0];
-            }
-            else {
-                currentTime = series1[i][0];
+        int i=0;
+        int j=0;
+        while(i<n || j<m){
+            int currtime=0;
+            if(j==m || (i<n && series1[i][0]< series2[j][0])){
+                currtime=series1[i][0];
+            }else if (i==n || (j<m && series1[i][0] > series2[j][0])){
+                currtime=series2[j][0];
+            }else {
+                currtime=series1[i][0];
             }
 
-            int value1 = (i < n) ? series1[i][1] : 0;
-            int value2 = (j < m) ? series2[j][1] : 0;
+            int value1= (i<n)?series1[i][1]: 0;
+            int value2= (j<m )? series2[j][1]:0;
+            ans.push_back({currtime,value1+value2});
 
-            ans.push_back({currentTime, value1 + value2});
 
-            if (i < n && series1[i][0] == currentTime)
+            if (i<n && series1[i][0]==currtime)
                 i++;
 
-            if (j < m && series2[j][0] == currentTime)
+            if (j<m && series2[j][0]==currtime)
                 j++;
         }
-
         return ans;
     }
 };
